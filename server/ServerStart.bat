37e1b48c30fc9f52cd91e730fc5eac87e4a9c517
Wes Fournier <wes.fournier@gmail.com>
2014-05-12 17:20:13 -0400

    Added mcpc for starterkit

---
+++ server/ServerStart.bat
@@ -0,0 +1,3 @@
+java -Xms2048m -Xmx2048m -XX:PermSize=128m -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -jar mcpc-plus-1.6.4-R2.1-forge965-B262.jar nogui
+:: java -Xms2048m -Xmx2048m -XX:PermSize=128m -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -jar FTBServer-1.6.4-965.jar nogui
+pause
